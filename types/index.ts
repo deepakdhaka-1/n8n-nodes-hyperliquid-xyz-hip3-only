9773b255a606d2cd8b680a52b148f29a9a986e72
DEEPAK DHAKA <124486673+deepakdhaka-1@users.noreply.github.com>
2026-01-14 18:32:25 +0530

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export interface Meta {
+  universe: Array<{ name: string }>;
+}
+
+export interface HyperliquidOrderWire {
+  a: number;
+  b: boolean;
+  p: string;
+  s: string;
+  r: boolean;
+  t: unknown;
+}
